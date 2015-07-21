d35f5083fd57189e118454bcffd437f8dec41dc2
Skyler Sauer <skyler@allen-sauer.com>
2015-07-20 20:48:14 -0700

    initial import

---
+++ Assets/MacheteController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MacheteController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3 (0f, 5f, 0f) * Time.deltaTime;
+
+		float angle = 300f * Time.time; 
+		transform.rotation = Quaternion.Euler (new Vector3 (0f, 0f, angle));
+	}
+}
---
+++ Assets/SpawnController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnController : MonoBehaviour {
+	public GameObject prefab;
+
+	void Start () {
+		InvokeRepeating ("Spawn", 0f, 1f);
+	}
+	
+	void Spawn () {
+		Instantiate (prefab, transform.position, transform.rotation);
+	}
+}
