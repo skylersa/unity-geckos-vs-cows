9c94b9ef6ac19378c45da1de5fc5c9d7ea71d5c5
Skyler Sauer <skyler@allen-sauer.com>
2015-08-06 18:59:56 -0700

    place gecko on click

---
+++ Assets/___Scripts/GeckoPlacementController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeckoPlacementController : MonoBehaviour {
+
+	public GameObject macheteGeckoPrefab;
+
+	Plane playerPlane;
+
+	void Start () {
+		playerPlane = new Plane (Vector3.forward, transform.position);
+	}
+	
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			float distance;
+			playerPlane.Raycast (ray, out distance);
+			Vector3 pos = ray.GetPoint(distance);
+			pos.z = transform.position.z;
+			Instantiate(macheteGeckoPrefab, pos,Quaternion.identity);
+		}
+
+	}
+}
