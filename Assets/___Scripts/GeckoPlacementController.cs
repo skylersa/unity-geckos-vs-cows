58b4f89a5399468d923d3e514280cc19ba40dc88
Skyler Sauer <skyler@allen-sauer.com>
2015-08-22 11:49:07 -0700

    working side panel

--- Assets/___Scripts/GeckoPlacementController.cs
+++ Assets/___Scripts/GeckoPlacementController.cs
@@ -3,26 +3,27 @@ using System.Collections;
 
 public class GeckoPlacementController : MonoBehaviour {
 
-	public GameObject macheteGeckoPrefab;
+	private GameObject geckoPrefab;
 
 	Plane playerPlane;
+
+	public void SelectGecko (GameObject geckoPrefab){
+		this.geckoPrefab = geckoPrefab;
+	}
 
 	void Start () {
 		playerPlane = new Plane (Vector3.forward, transform.position);
 	}
 	
-	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
-			if (MoneyTextController.AvaialbleMoney() >= 300) {
-				MoneyTextController.AddMoney(-300);
-				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-				float distance;
-				playerPlane.Raycast (ray, out distance);
-				Vector3 pos = ray.GetPoint(distance);
-				pos.z = transform.position.z;
-				Instantiate(macheteGeckoPrefab, pos,Quaternion.identity);
-			}
+	public void PlaceGecko () {
+		if (geckoPrefab != null && MoneyTextController.AvaialbleMoney() >= 300) {
+			MoneyTextController.AddMoney(-300);
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			float distance;
+			playerPlane.Raycast (ray, out distance);
+			Vector3 pos = ray.GetPoint(distance);
+			pos.z = transform.position.z;
+			Instantiate(geckoPrefab, pos, Quaternion.identity);
 		}
-
 	}
 }
