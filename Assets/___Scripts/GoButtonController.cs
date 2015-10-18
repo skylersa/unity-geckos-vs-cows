97f8883d44ef7728469e23bfa4a7b5ad8a70dfba
Skyler Sauer <skyler@allen-sauer.com>
2015-10-18 13:18:39 -0700

    only starts round when go button is press

--- Assets/___Scripts/GoButtonController.cs
+++ Assets/___Scripts/GoButtonController.cs
@@ -1,12 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GoButtonController : MonoBehaviour {
+public class GoButtonController : MonoBehaviour
+{
 
+	public GameObject spawns;
 	private int speedLevel;
 
-	public void ClickGoButton () {
+	void Start ()
+	{
+		spawns.SetActive (false);
+	}
+
+	public void ClickGoButton ()
+	{
+		spawns.SetActive (true);
 		speedLevel = (speedLevel + 1) % 4;
-		Time.timeScale = Mathf.Pow(2, speedLevel);
+		Time.timeScale = Mathf.Pow (2, speedLevel);
 	}
 }
