0e4bad8e112736a67b24ee783c23a59b55e72c99
Skyler Sauer <skyler@allen-sauer.com>
2015-11-28 19:45:47 -0800

    basic levelling

--- Assets/___Scripts/GameController.cs
+++ Assets/___Scripts/GameController.cs
@@ -4,29 +4,54 @@ using System.Collections;
 
 public class GameController : MonoBehaviour
 {
+	public GameObject cowSpawnPrefab;
 	public Text leveltext;
 
-	SpawnController[] cowSpawns;
 	int lvl;
 
+	void Start ()
+	{
+		leveltext.text = "";
+	}
+
+	void CreateSpawn (int x, int y, int spawnCount)
+	{
+		GameObject clone = Instantiate (cowSpawnPrefab);
+		clone.transform.SetParent (transform, false);
+		clone.transform.position = new Vector2 (x, y);
+		SpawnController controller = clone.GetComponent<SpawnController> ();
+		controller.spawncount = spawnCount;
+	}
+	
 	public void NextLevel ()
 	{
 		lvl++;
 		leveltext.text = "Lvl " + lvl;
 
-		foreach (SpawnController controller in cowSpawns) {
-			controller.spawncount = lvl;
-		}
+		CreateSpawn (-9, 1, lvl);
+		CreateSpawn (-9, -1, lvl);
+
+
+		InvokeRepeating ("CheckLevelOver", 1f, 1f);
 	}
 
-	void Start ()
+	void CheckLevelOver ()
 	{
-		leveltext.text = "";
-		cowSpawns = GetComponentsInChildren<SpawnController> ();
+		foreach (Transform cowSpawn in transform) {
+			SpawnController controller = cowSpawn.GetComponent<SpawnController> ();
+			if (!controller.IsEntirelyDone ()) {
+				// found a controller that isn't done yet
+				return;
+			}
+		}
+
+		// all controllers are done
+		LevelOver ();
 	}
-	
-	void Update ()
+
+	void LevelOver ()
 	{
-	
+		Time.timeScale = 0;
 	}
+
 }
--- Assets/___Scripts/SpawnController.cs
+++ Assets/___Scripts/SpawnController.cs
@@ -9,26 +9,42 @@ public class SpawnController : MonoBehaviour
 	public float rate = 1f;
 	public float velocity = 1f;
 
-	private int spawned;
+	int spawned;
+
 	void Start ()
 	{
 		InvokeRepeating ("Spawn", 0f, rate);
 	}
-	
+
+	public bool IsEntirelyDone ()
+	{
+		return IsDoneSpawning () && transform.childCount == 0;
+	}
+
+	bool IsDoneSpawning ()
+	{
+		return spawned >= spawncount;
+	}
+
 	void Spawn ()
 	{
 		if (!enabled) {
 			return;
 		}
-		if (++spawned > spawncount) {
+
+		if (IsEntirelyDone ()) {
 			StopAllCoroutines ();
+			Destroy (gameObject);
 			return;
 		}
 
-		GameObject clone = Instantiate (prefab [0], transform.position, transform.rotation) as GameObject;
-		clone.transform.parent = transform;
-		clone.transform.rotation = Quaternion.identity;
-		Rigidbody2D rb = clone.GetComponent<Rigidbody2D> ();
-		rb.velocity = transform.up * velocity;
+		if (!IsDoneSpawning ()) {
+			++spawned;
+			GameObject clone = Instantiate (prefab [0], transform.position, transform.rotation) as GameObject;
+			clone.transform.parent = transform;
+			clone.transform.rotation = Quaternion.identity;
+			Rigidbody2D rb = clone.GetComponent<Rigidbody2D> ();
+			rb.velocity = transform.up * velocity;
+		}
 	}
 }
